70c2c3454082b7afa089189b4af189e303db9af4
Amber Koval <amberkoval92@gmail.com>
2019-05-09 22:59:35 -0700

    push file structure

---
+++ letters.js
@@ -0,0 +1,19 @@
+var inquirer = require('inquirer');
+
+var letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
+var guessed = false;
+
+
+
+
+
+
+inquirer .prompt([
+        console.log(// pass questions in here)
+    ])
+    .then(answers => {
+        
+        console.log(// user feedback)
+            
+        );
+    });
